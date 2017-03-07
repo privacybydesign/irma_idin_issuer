510356de9448bd2ac4dfb7ab86f8c01722b8dcf7
Sietse Ringers <mail@sietseringers.net>
2017-03-07 18:02:43 +0100

    Expand start method

--- src/main/java/org/irmacard/idin/web/IdinApplication.java
+++ src/main/java/org/irmacard/idin/web/IdinApplication.java
@@ -1,16 +1,28 @@
 package org.irmacard.idin.web;
 
+import net.bankid.merchant.library.Configuration;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
 
 import javax.ws.rs.ApplicationPath;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URL;
 
 @ApplicationPath("/")
 public class IdinApplication extends ResourceConfig {
 	private static Logger logger = LoggerFactory.getLogger(IdinApplication.class);
 
-	public IdinApplication() {
+	public IdinApplication() throws IOException, ParserConfigurationException, SAXException {
+		URL config = IdinApplication.class.getClassLoader().getResource("config.xml");
+		if (config == null)
+			throw new RuntimeException("Could not load config.xml");
+		Configuration.defaultInstance().Load(config.openStream());
+
 		register(IdinResource.class);
+
+		logger.info("Starting IRMA iDIN server");
 	}
 }
--- src/main/java/org/irmacard/idin/web/IdinResource.java
+++ src/main/java/org/irmacard/idin/web/IdinResource.java
@@ -1,15 +1,43 @@
 package org.irmacard.idin.web;
 
-import javax.ws.rs.*;
+import net.bankid.merchant.library.*;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.math.BigInteger;
+import java.util.Random;
 
 @Path("v1/idin")
 public class IdinResource {
-	@GET
+	public final static int IDIN_ATTRIBUTES = ServiceIds.Address
+			| ServiceIds.ConsumerBin
+			| ServiceIds.DateOfBirth
+			| ServiceIds.Email
+			| ServiceIds.Gender
+			| ServiceIds.Telephone
+			| ServiceIds.Name;
+
+	private static Random random = new Random();
+
+	@POST
 	@Path("/start")
-	public Response start() {
-		// TODO
-		return Response.accepted("OK").build();
+	@Consumes(MediaType.TEXT_PLAIN)
+	public Response start(String bank) {
+		// Create request
+		String transactionId = new BigInteger(130, random).toString(32);
+		String merchantReference = new BigInteger(130, random).toString(32);
+		AuthenticationRequest request = new AuthenticationRequest(
+				transactionId, IDIN_ATTRIBUTES, bank, AssuranceLevel.Loa3, null, merchantReference);
+
+		// Execute request
+		AuthenticationResponse response = new Communicator().newAuthenticationRequest(request);
+
+		// Handle request result
+		if (response.getIsError())
+			throw new RuntimeException(response.getErrorResponse().getConsumerMessage());
+		return Response.accepted(response.getIssuerAuthenticationURL()).build();
 	}
 }
