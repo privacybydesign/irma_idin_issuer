99753927eaa9eccb7a11fd9d543864b1bdb7b788
Fabian <f.vandenbroek@cs.ru.nl>
2017-03-22 17:27:35 +0100

    WIP still to add: attributes & JWT in Cookies.

--- src/main/java/org/irmacard/idin/web/IdinResource.java
+++ src/main/java/org/irmacard/idin/web/IdinResource.java
@@ -2,15 +2,16 @@ package org.irmacard.idin.web;
 
 import net.bankid.merchant.library.*;
 import net.bankid.merchant.library.internal.DirectoryResponseBase;
+import org.slf4j.*;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
+import javax.ws.rs.QueryParam;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
 
 @Path("v1/idin")
 public class IdinResource {
@@ -23,6 +24,10 @@ public class IdinResource {
 			| ServiceIds.Name;
 
 	private static Random random = new Random();
+	private static Logger logger = LoggerFactory.getLogger(IdinResource.class);
+
+	private static String returnURL = "http://localhost:8080/irma_idin_server/enroll.html";
+	private boolean isHttpsEnabled = false;
 
 	@GET
 	@Path("/banks")
@@ -33,13 +38,17 @@ public class IdinResource {
 
 	@POST
 	@Path("/start")
-	@Consumes(MediaType.TEXT_PLAIN)
-	public Response start(String bank) {
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response start (String bank){
+		logger.warn("called it!");
+		logger.warn(bank);
+
 		// Create request
-		String transactionId = new BigInteger(130, random).toString(32);
 		String merchantReference = new BigInteger(130, random).toString(32);
-		AuthenticationRequest request = new AuthenticationRequest(
-				transactionId, IDIN_ATTRIBUTES, bank, AssuranceLevel.Loa3, null, merchantReference);
+		//iDIN lib wants the random MerchantReference to start with a letter.
+		merchantReference = "a"+merchantReference;
+		logger.error(">>>" + merchantReference);
+		AuthenticationRequest request = new AuthenticationRequest("successHIO100OIHtest",IDIN_ATTRIBUTES,bank,AssuranceLevel.Loa3,"nl",merchantReference);
 
 		// Execute request
 		AuthenticationResponse response = new Communicator().newAuthenticationRequest(request);
@@ -48,5 +57,38 @@ public class IdinResource {
 		if (response.getIsError())
 			throw new RuntimeException(response.getErrorResponse().getConsumerMessage());
 		return Response.accepted(response.getIssuerAuthenticationURL()).build();
+	}
+
+	@GET
+	@Path("/return")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response authenticated(@DefaultValue("error") @QueryParam("trxid") String trxID){
+		NewCookie[] cookies = new NewCookie[1];
+
+		if (trxID.equals("error") ){
+			//TODO show error page
+		}
+		StatusRequest sr = new StatusRequest(trxID);
+		StatusResponse response = new Communicator().getResponse(sr);
+
+		if (response.getIsError()){
+			logger.error(response.getErrorResponse().getErrorMessage());
+			//TODO: show error page to user
+		} else if (response.getStatus().equals(StatusResponse.Success)){
+			//redirect to issuing page
+			Map<String, String> attributes = response.getSamlResponse().getAttributes();
+			cookies[0] = new NewCookie("city", attributes.get("urn:nl:bvn:bankid:1.0:consumer.city"),"/",null,null,60, isHttpsEnabled);
+			//for (Map.Entry<String, String> entry : attributes.entrySet())
+			//{
+			//	logger.info(entry.getKey() + "/" + entry.getValue());
+			//}
+		}
+		try {
+			URI issueURI = new URI(returnURL);
+			return Response.seeOther(issueURI).cookie(cookies).build();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
